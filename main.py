cfba8ace4a118490f98f7e78b1e22466c95955c3
Vladislav98 <vlados.vidos@mail.ru>
2022-06-02 18:50:10 +0300

    Add file

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class TestMySecondTest:
+    def test_post_users(self):
+        response_body = requests.request('post', 'https://reqres.in/api/users').json()
+        print(response_body)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
